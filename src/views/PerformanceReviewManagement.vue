a186c6b5ef1605361eab3c9615de02145325a70d
khoa-k2 <khoavu.vndk@gmail.com>
2026-09-02 18:08:00 +0700

    update lai nut delete

--- src/views/PerformanceReviewManagement.vue
+++ src/views/PerformanceReviewManagement.vue
@@ -487,7 +487,7 @@
 
                   <!-- Delete -->
                   <button
-                      v-if="review.status !== 'COMPLETED'"
+                      v-if="review.status === 'DRAFT'"
                       type="button"
                       @click="openDeleteConfirm(review)"
                       class="inline-flex items-center justify-center gap-2
